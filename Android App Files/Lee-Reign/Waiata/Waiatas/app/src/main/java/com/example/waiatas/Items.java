b1a9b38d7284370590ebfdaf9c54d603f9f96448
Timothy <timhow38@gmail.com>
2020-06-05 18:30:07 +1200

    Commit

---
+++ Android App Files/Lee-Reign/Waiata/Waiatas/app/src/main/java/com/example/waiatas/Items.java
@@ -0,0 +1,17 @@
+package com.example.waiatas;
+
+public class Items {
+    private int mImageResource;
+    private String mTitle;
+
+
+    public Items (int imageResource, String title) {
+        mImageResource = imageResource;
+        mTitle = title;
+    }
+
+    public int getmImageResource() {
+        return mImageResource;
+    }
+
+}
