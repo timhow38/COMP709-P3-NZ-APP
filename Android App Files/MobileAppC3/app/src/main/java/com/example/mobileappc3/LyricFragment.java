b43094830194e707ded85a8dc6cd66e9642f2e85
zR31GN <57340437+zR31GN@users.noreply.github.com>
2020-06-14 14:35:03 +1200

    Added Lryic Fragment and Waiata Player with scroll fragment

---
+++ Android App Files/MobileAppC3/app/src/main/java/com/example/mobileappc3/LyricFragment.java
@@ -0,0 +1,23 @@
+package com.example.mobileappc3;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class LyricFragment extends Fragment {
+
+    public LyricFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_lyrics, container, false);
+    }
+}
---
+++ Android App Files/MobileAppC3/app/src/main/java/com/example/mobileappc3/waiataScrollFragment.java
@@ -0,0 +1,23 @@
+package com.example.mobileappc3;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class waiataScrollFragment extends Fragment {
+
+    public waiataScrollFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.waiata_scroll_frag, container, false);
+    }
+}
