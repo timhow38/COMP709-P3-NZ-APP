12bce558c3e68178dd992f0a3ca2bbe11114e1d7
RforL <RforL@users.noreply.github.com>
2020-06-15 23:50:47 +1200

    Major Update
    Updates to Splash Page (on timer)
    Updates to MainMenu
    Refactors Galore
    Link to About page

---
+++ Android App Files/MobileAppC3/app/src/main/java/com/example/mobileappc3/About.java
@@ -0,0 +1,16 @@
+package com.example.mobileappc3;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class About extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+    }
+}
